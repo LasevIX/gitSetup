30aa2a4f7f60dc9d8422248109303379b888ea44
LasevIX_@Laptop-lOhkM <l73yx.off@gmail.com>
2022-03-23 20:06:59 +0100

    added print statement to main.py

--- main.py
+++ main.py
@@ -2,12 +2,13 @@
 def main():
     for i in range(0,11):
         print(32/2*5*i)
-<<<<<<< HEAD
-=======
         print("\n")
     file = open("NewFile.txt","w")
     file.write("Hello there!")
     file.close()
->>>>>>> branchONe
+    filer = open("Newfile.txt", "r")  #very convoluted print statement...
+    fileContent =filer.read()
+    print(fileContent)
+    filer.close()
 #boilerplate
 main()
